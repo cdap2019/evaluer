203f9dc5cae7458d379ad4fce6a9bc4eaeda6d1d
Supun Dileepa <dilee1st.1995@gmail.com>
2019-08-25 08:47:54 +0530

    land model added

--- models/land.js
+++ models/land.js
@@ -1,21 +1,41 @@
 const mongoose = require('mongoose');
 
-
 // Land Schema
 const LandSchema = mongoose.Schema ({
-  name: {
-    type: String
+  price: {
+    type: Number,
+    required: true
   },
-  email: {
+  distance: {
+    type: Number,
+    required: true
+  },
+  route: {
     type: String,
     required: true
   },
-  username: {
+  index: {
+    type: Number,
+    required: true
+  },
+  province:{
     type: String,
     required: true
   },
-  password: {
+  district:{
+    type: Number,
+    required: true
+  },
+  address:{
     type: String,
+    required: true
+  },
+  lat:{
+    type: Number,
+    required: true
+  },
+  lon:{
+    type: Number,
     required: true
   }
 });
