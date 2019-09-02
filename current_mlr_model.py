b9462f7931e58e7655b21ac1453ab60b9b94b8d0
Bimali-YMY <b.yapabmmv@gmail.com>
2019-09-02 20:36:15 +0530

    added mlr model and lstm

--- current_mlr_model.py
+++ current_mlr_model.py
@@ -6,9 +6,9 @@ from sklearn.preprocessing import StandardScaler
 
 
 
-house_data = pd.read_csv('./kc_house_data.csv')
-print(house_data.shape)
-print(house_data.head())
+land_data = pd.read_csv('./data.csv')
+print(land_data.shape)
+print(land_data.head())
 
 
 fig_size = plt.rcParams['figure.figsize']
@@ -16,34 +16,20 @@ fig_size[0] = 10
 fig_size[1] = 8
 plt.rcParams['figure.figsize'] = fig_size
 
-sns.distplot(house_data['price'], kde=False, bins=8)
-sns.lineplot(x='sqft_living', y='price', data=house_data)
-sns.barplot(x='bedrooms', y='price', data=house_data)
-sns.barplot(x='waterfront', y='price', data=house_data)
-sns.lineplot(x='yr_built', y='price', data=house_data)
-sns.lineplot(x='grade', y='price', data=house_data)
+sns.distplot(land_data['price'], kde=False, bins=8)
+sns.lineplot(x='dtmr', y='price', data=land_data)
+sns.barplot(x='extent', y='price', data=land_data)
 
 
-dataset_features = house_data.drop(['price', 'id', 'date'], axis=1)
+dataset_features = land_data.drop(['price', 'id', 'date'], axis=1)
 
-dataset_labels = house_data['price']
+dataset_labels = land_data['price']
 
 
 from sklearn.model_selection import train_test_split
 
 train_features,test_features,train_labels,test_labels = \
     train_test_split(dataset_features,dataset_labels,test_size=0.25, random_state=21)
-
-# sc = StandardScaler()
-#
-# train_features = sc.fit_transform(train_features)
-# test_features = sc.transform(test_features)
-
-# train_labels = train_labels.values.reshape(-1,1)
-# train_labels = sc.fit_transform(train_labels)
-#
-# test_labels = test_labels.values.reshape(-1,1)
-# test_labels = sc.transform(test_labels)
 
 
 from sklearn.linear_model import LinearRegression
